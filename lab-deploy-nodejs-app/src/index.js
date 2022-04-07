8ef7d9f5542a45a5975dc892602aabf60f5c32d6
Rio Astamal <me@rioastamal.net>
2022-04-07 16:00:44 +0000

    src/index: Add new attribute .network

--- lab-deploy-nodejs-app/src/index.js
+++ lab-deploy-nodejs-app/src/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 8080;
+const { networkInterfaces } = require('os');
 
 app.set('json spaces', 2);
 app.get('/', function mainRoute(req, res) {
+  const network = networkInterfaces();
+  delete network['lo']; // remove loopback interface
+
   const mainResponse = {
-    "hello": "Indonesia Belajar!"
+    "hello": "Indonesia Belajar!",
+    "network": network
   };
-  
+
   res.json(mainResponse);
 });
 
